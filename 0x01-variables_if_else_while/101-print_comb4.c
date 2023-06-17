07108875c86f7cd70b6dd8bfd5d603d8c65179ee
AbdulrahmanGhanem <abdulrahmanmagdy014@gmail.com>
2023-06-17 05:33:45 +0300

    Task 101 O

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints all combinations of 3 digits
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int dgt1, dgt2, dgt3;
+
+	for (dgt1 = 0; dgt1 <= 9; dgt1++)
+	{
+		for (dgt2 = 0; dgt2 <= 9; dgt2++)
+		{
+			for (dgt3 = 0; dgt3 <= 9; dgt3++)
+			{
+				if (dgt1 != dgt2 != dgt3 && dgt1 < dgt2 < dgt3)
+				{
+					putchar('0' + dgt1);
+					putchar('0' + dgt2);
+					if (dgt1 + dgt2 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
