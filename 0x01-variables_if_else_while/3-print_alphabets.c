afcb4f5720ba6847cbb174caf52bd321db80f0f8
AbdulrahmanGhanem <abdulrahmanmagdy014@gmail.com>
2023-06-17 03:26:18 +0300

    Task 3 M

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints Alphabets lower & upper case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter, LETTER;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (LETTER = 'Z'; LETTER <= 'Z'; LETTER++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
