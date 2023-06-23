02395f5a98e4d106af53746222847f77f520671e
AbdulrahmanGhanem <abdulrahmanmagdy014@gmail.com>
2023-06-23 13:05:08 +0300

    Task 5 M

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,13 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
+			num = c;
 			if (c > 9)
 			{
 				_putchar(49);
 				num = c % 10;
 			}
-			_putchar('0' + c);
+			_putchar('0' + num);
 		}
 		_putchar('\n');
 	}
