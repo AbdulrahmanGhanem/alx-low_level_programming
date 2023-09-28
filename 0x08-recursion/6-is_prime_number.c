7faf274076e9e77c3110fde07cebd5d9276fb50d
AbdulrahmanGhanem <abdulrahmanmagdy014@gmail.com>
2023-09-29 02:51:31 +0300

    Task 6 m

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,7 +29,7 @@ int if_prime(int n, int x)
 {
 	if (x >= n && n > 1)
 		return (1);
-	else if (n % x== 0 || n <= 1)
+	else if (n % x == 0 || n <= 1)
 		return (0);
 	else
 		return (if_prime(n, x + 1));
